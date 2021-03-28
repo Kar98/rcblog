380e00b5abf7d82bae03c74d940b6559b39d32dd
Rory Crickmore <rory.crickmore@kjross.com.au>
2021-03-28 11:29:50 +1100

    initial commit

---
+++ .powershell setup.ps1
@@ -0,0 +1,3 @@
+.\venv\Scripts\Activate.ps1
+$env:FLASK_APP='app.py'
+$env:FLASK_ENV='development'
---
+++ pyfiles/pagelogic.py
@@ -0,0 +1,6 @@
+
+def valid_login(username,password):
+    return False
+
+def do_login(username):
+    return False
