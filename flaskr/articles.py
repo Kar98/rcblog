6a37ac61c267876258d41df058be9c4475668391
Rory Crickmore <rory.crickmore@kjross.com.au>
2021-06-11 16:04:20 +1000

    Added 2 more articles

--- flaskr/articles.py
+++ flaskr/articles.py
@@ -12,3 +12,11 @@ def articleIndex():
 @bp.route('/articles/2021/agile')
 def article1():
     return render_template('articles/agile.html')
+
+@bp.route('/articles/2021/automation-ins-and-outs')
+def article2():
+    return render_template('articles/automation-ins-and-outs.html')
+
+@bp.route('/articles/2021/running-a-good-review')
+def article3():
+    return render_template('articles/running-a-good-review.html')
