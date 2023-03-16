eb934212ee04f26082e71c3dc3b440eae195e4b5
Rory <rory.crickmore@kablamo.com.au>
2023-03-17 08:03:56 +1100

    Added static file handling

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, render_template, g
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_url_path='', static_folder='static')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
